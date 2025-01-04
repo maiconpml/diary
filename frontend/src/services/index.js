ab0cd5edb35267b7b538c2d28f57ef074c65267e
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2025-01-04 11:05:35 -0300

    feat: create api calls to access api endpoints

---
+++ frontend/src/services/index.js
@@ -0,0 +1,3 @@
+import { login, logout, register, getAuth} from './requests'
+
+export { login, logout, register, getAuth }
---
+++ frontend/src/services/requests.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://127.0.0.1:8000/api/'
+
+const api = axios.create({
+    baseURL:BASE_URL,
+    withCredentials:true,
+})
+
+api.interceptors.response.use(
+    (response) => response,
+    async error => {
+        const original_request = error.config
+
+        if(error.response?.status === 401 && !original_request._retry) {
+            original_request._retry = true
+
+            try {
+                await refreshToken()
+                return api(original_request)
+            }catch(refreshError){
+                window.location.href = '/login'
+                return Promise.reject(refreshError)
+            }
+        }
+        return Promise.reject(error)
+    }
+)
+
+const refreshToken = async () => {
+    const response = await api.post('/token/refresh/')
+    return response.data
+}
+
+export const login = async (username, password) => {
+    const response = await api.post('/token/', {username, password})
+    return response.data
+}
+
+export const register = async (username, email, firstName, lastName, password) => {
+    const response = await api.post('/register/', {username:username, email:email, first_name:firstName, last_name:lastName, password:password})
+    return response.data
+}
+
+export const logout = async () => {
+    const response = await api.post('/logout/')
+    return response.data
+}
+
+export const getAuth = async () => {
+    const response = await api.post('/authenticated/')
+    console.log(response)
+    return response.data
+}

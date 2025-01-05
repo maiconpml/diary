5ce9464df9ebdcd77486919c6a54edf1362b5718
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2025-01-05 17:05:14 -0300

    refactor: clean Login and register
    Make both only form components

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -22,42 +22,23 @@ const Login = () => {
         }
     }
 
-
     return(
         <>
-            <div className="min-h-screen text-[--fontprimarycol]" style={{backgroundImage: `url(${background})`, backgroundRepeat: "no-repeat", backgroundSize: "100% 100%"}}>
-                <div className=" h-screen flex items-center justify-center ">
-                    <div className="flex items-center justify-center rounded-md shadow-full w-1/2 h-1/2 min-w-min overflow-clip">
-                        <div className="relative bg-transparent backdrop-blur-lg p-10 w-1/2 h-full min-w-min">
-                            <h1 className="text-6xl antialiased font-bold">DIARY</h1>
-                            <span className="text-4xl ">Never forget a commitment again.</span><br/>
-                            <span className="absolute bottom-3 text-xl">Coming soon to mobile!</span>
-                        </div>
-                        <div className="relative flex flex-col justify-center items-center p-10 w-1/2 h-full min-w-min max-w-full bg-[--primarycol]">
-                            <form className="flex flex-col justify-center items-center w-full  " onSubmit={handleLogin}>
-                            
-
-                                <label className="w-3/4 text-lg">Username:<br/>
-                                <input type="text" name="username-input" placeholder="Your username" onChange={(e) => {setUsername(e.target.value)}} className="bg-[--primarycol] text-lg p-2 w-full mt-1 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
-
-                                <label className="w-3/4 text-lg">Password:<br/>
-                                <input type="password" name="password-input" placeholder="••••••••" onChange={(e) => {setPassword(e.target.value)}} className="bg-[--primarycol] text-lg p-2 w-full mt-1 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
-
-                                <button type="submit" className="text-lg bg-[--fontprimarycol] text-[--primarycol] px-8 py-3 rounded-md">Login</button> 
-
-
-                            </form>
-                            <button className="absolute bottom-3 text-lg mt-3" onClick={(e) => {
-                                e.preventDefault 
-                                navigate('/register')}}>
-                                Doesn't have an account? Signup
-                            </button>
-                        </div>
-                        
-                    </div>
-                    
-                </div>
+            <div className="relative flex flex-col justify-center items-center p-10 w-1/2 h-full min-w-[50%] max-w-full bg-[--primarycol] text-lg">
+                <form className="flex flex-col justify-center items-center w-full" onSubmit={handleLogin}>
                 
+                    <label className="w-3/4 ">Username:<br/>
+                    <input type="text" name="username-input" placeholder="Your username" onChange={(e) => {setUsername(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-1 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
+
+                    <label className="w-3/4 ">Password:<br/>
+                    <input type="password" name="password-input" placeholder="••••••••" onChange={(e) => {setPassword(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-1 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
+
+                    <button type="submit" className=" bg-[--fontprimarycol] text-[--primarycol] px-8 py-3 rounded-md">Login</button> 
+
+                            <button className="absolute bottom-3 text-lg mt-3" onClick={(e) => {
+                </form>
+                    Doesn't have an account? Signup
+                </button>
             </div>
         </>
     )
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,25 +1,28 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { register } from "../services"
-import background from '../../public/praia2-bg.jpg'
 
 
 const Register = () => {
     const [username, setUsername] = useState("")
     const [email, setEmail] = useState("")
     const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
-    const navigate = useNavigate()
 
     const handleRegister = async (e) => {
         e.preventDefault()
         if(password === confirmPassword){
             try{
-                await register(username, email, firstName, lastName, password)
+                await register(username, email, firstName, password)
                 alert('Register sucessful')
                 navigate('/login')
+                setUsername("")
+                setEmail("")
+                setFirstName("")
+                setPassword("")
+                setConfirmPassword("")
+                handleChangeForm()
             }catch{
                 alert('Error in registration')
             }
@@ -30,44 +33,28 @@ const Register = () => {
 
     return(
         <>
-            <div className="min-h-screen text-[--fontprimarycol]" style={{backgroundImage: `url(${background})`, backgroundRepeat: "no-repeat", backgroundSize: "100% 100%"}}>
-                <div className=" h-screen flex items-center justify-center ">
-                    <div className="flex items-center justify-center rounded-md shadow-full w-3/5 h-3/4 min-w-min overflow-clip">
-                        <div className="relative flex flex-col justify-center items-center p-10 w-1/2 h-full min-w-min max-w-full bg-[--primarycol]">    
-                            <form onSubmit={handleRegister} className="flex flex-col justify-center items-center w-full text-base ">
-                                <label className="w-3/4 ">Username:<br/>
-                                <input type="text" placeholder="Your username" onChange={(e) => {setUsername(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
-                                
-                                <label className="w-3/4 ">Email:<br/>
-                                <input type="email" placeholder="e. g. email@email.com" onChange={(e) => {setEmail(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
+            <div className="relative flex flex-col justify-center items-center p-10 w-1/2 h-full min-w-[50%] max-w-full bg-[--primarycol] text-lg">    
+                <form onSubmit={handleRegister} className="flex flex-col justify-center items-center w-full">
+                    <label className="w-3/4 ">Username:<br/>
+                    <input value={username} type="text" placeholder="Your username" onChange={(e) => {setUsername(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
+                    
+                    <label className="w-3/4 ">Email:<br/>
+                    <input value={email} type="email" placeholder="e. g. email@email.com" onChange={(e) => {setEmail(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
 
-                                <label className="w-3/4 ">First Name:<br/>
-                                <input type="text" placeholder="Your first name" onChange={(e) => {setFirstName(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
+                    <label className="w-3/4 ">First Name:<br/>
+                    <input value={firstName} type="text" placeholder="Your first name" onChange={(e) => {setFirstName(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
 
-                                <label className="w-3/4 ">Last Name:<br/>
-                                <input type="text" placeholder="Your last name" onChange={(e) => {setLastName(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
+                    <label className="w-3/4 ">Password:<br/>
+                    <input value={password} type="password" placeholder="••••••••" onChange={(e) => {setPassword(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
 
-                                <label className="w-3/4 ">Password:<br/>
-                                <input type="password" placeholder="********" onChange={(e) => {setPassword(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
+                    <label className="w-3/4 ">Confirm Password:<br/>
+                    <input value={confirmPassword} type="password" placeholder="••••••••" onChange={(e) => {setConfirmPassword(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
 
-                                <label className="w-3/4 ">Confirm Password:<br/>
-                                <input type="password" placeholder="********" onChange={(e) => {setConfirmPassword(e.target.value)}} className="bg-[--primarycol]  p-2 w-full mt-0.5 border-b border-[--fontprimarycol] focus:outline-0"/></label><br/>
-
-                                <button type="submit" className=" bg-[--fontprimarycol] text-[--primarycol] px-8 py-3 rounded-md">Register</button> 
-                            </form>
-                            <button className="absolute bottom-3  mt-3" onClick={(e) => {
-                                e.preventDefault 
-                                navigate('/login')}}>
-                                Already have an account? Signin
-                            </button>
-                        </div>
-                        <div className="relative bg-transparent backdrop-blur-lg p-10 w-1/2 h-full min-w-min">
-                            <h1 className="text-6xl antialiased font-bold">DIARY</h1>
-                            <span className="text-4xl ">Never forget a commitment again.</span><br/>
-                            <span className="absolute bottom-3 text-lg ">Coming soon to mobile!</span>
-                        </div>
-                    </div>
-                </div>
+                    <button type="submit" className=" bg-[--fontprimarycol] text-[--primarycol] px-8 py-3 rounded-md">Register</button> 
+                </form>
+                <button className="absolute bottom-3 mt-3" onClick={handleChangeForm}>
+                    Already have an account? Signin
+                </button>
             </div>
         </>
     )

1629518112ab4464a44305f9099ed191cf136c18
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2025-01-05 17:00:43 -0300

    refactor: change user registration serializer
    Remove last name attribute

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -7,20 +7,17 @@ class MyUserProfileSerializer(serializers.ModelSerializer):
         model = MyUser
         fields = ['profile_image']
 
-
-
 class MyUserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = MyUser
-        fields=['username', 'email', 'first_name', 'last_name', 'password']
+        fields=['username', 'email', 'first_name', 'password']
 
     def create(self, validated_data):
         user = MyUser(
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
         )
         user.set_password(validated_data['password'])
         user.save()

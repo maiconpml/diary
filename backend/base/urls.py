da69291cb259ded955370ffeb2e7a485b794bf3f
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2025-01-06 17:00:29 -0300

    feat: create urls for get_appointments_by_date requests

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CustomTokenObtainPairView, CustomRefreshTokenView, logout, is_authenticated, register, create_appointment, update_appointment, get_appointments, debug, delete_appointment
+from .views import CustomTokenObtainPairView, CustomRefreshTokenView, logout, is_authenticated, register, create_appointment, update_appointment, get_appointments,  delete_appointment, get_appointments_by_date
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -14,4 +14,7 @@ urlpatterns = [
     path('appointments/create/', create_appointment),
     path('appointments/update/<int:pk>', update_appointment),
     path('appointments/delete/<int:pk>', delete_appointment),
+    path('appointments/<int:year>', get_appointments_by_date),
+    path('appointments/<int:year>/<int:month>', get_appointments_by_date),
+    path('appointments/<int:year>/<int:month>/<int:day>', get_appointments_by_date),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
